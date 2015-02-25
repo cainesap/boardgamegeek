baca7c287334f7cef267d44b4bd5214009c81235
cainesap <cainesap@gmail.com>
2015-02-25 22:02:15 +0000

    find 'num_players_best' with highest votes

--- populate_db.R
+++ populate_db.R
@@ -8,6 +8,27 @@ library(RSQLite)
 ## read csv file downloaded from URL
 bgg <- read.csv('boardgamegeek.csv', sep=";")
 
+## extract 'best' number of players
+bgg$num_players_best <- as.character(bgg$num_players_best)
+rows <- nrow(bgg)
+bestplyrs <- vector()
+for (r in (1:rows)) {
+  if (nchar(bgg$num_players_best[r]) > 0) {
+    allvotes <- as.numeric(unlist(strsplit(bgg$num_players_best[r], ",|:")))  # split string of voting info
+    votes <- allvotes[seq(2, length(allvotes), by = 2)]  # get the number of votes (the even numbered vector items)
+    optimal <- which(votes == max(votes))  # find the most popular number(s) of players for this game
+    if (length(optimal) > 1) {
+      bestplyrs[r] <- paste(min(optimal):max(optimal), collapse = ",")  # if several equal values, add as comma-separated list
+    } else {
+      bestplyrs[r] <-  optimal  # if single clear favourite
+    }
+  } else {
+    bestplyrs[r] <- 'unknown'  # if no votes registered
+  }
+}
+bgg$best_num_players <- bestplyrs
+
+
 ## open a new db connection
 my_db <- src_sqlite("bgg.sqlite3", create=T)
 
